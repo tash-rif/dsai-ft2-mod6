537ec7955227a1b6680f7ca725ba1830965348d7
tash-rif <tashrif@gmail.com>
2026-04-23 04:39:15 +0000

    comment out GROQ_API_KEY

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import datetime
 
 # for AWS, do not run this because not using .env
 #os.environ["GROQ_API_KEY"] = ""
-os.environ["GROQ_API_KEY"] = os.environ.get('GROQ_API_KEY')
+#os.environ["GROQ_API_KEY"] = os.environ.get('GROQ_API_KEY')
 
 client = Groq()
 
